01f95d656f2c00adb3548d0afb4e77276fea102a
veronika.cherkashchenko <veronika.cherkashchenko@mythical.games>
2025-10-23 11:53:46 +0200

    pass check

--- src/test/java/tests/player/get/GetPlayerNegativeTests.java
+++ src/test/java/tests/player/get/GetPlayerNegativeTests.java
@@ -21,4 +21,7 @@ public class GetPlayerNegativeTests extends BasePlayerTest {
         //todo cannot be deployed due to lack of user mention in request
     }
 
+    //TODO also there should be such tests if we would have user authoization and authication
+    // admin can't get by id supervisor and another admin
+    // user can't get by id admin supervisor and another user
 }
--- src/test/java/tests/player/get/GetPlayerPositiveTests.java
+++ src/test/java/tests/player/get/GetPlayerPositiveTests.java
@@ -12,4 +12,9 @@ public class GetPlayerPositiveTests extends BasePlayerTest {
     }
 
     //TODO delete 201 code created from spec
+
+    //TODO also there should be such tests if we would have user authoization and authication
+    // supervisor can get by id supervisor
+    // admin can get by id user and admin (him/herself)
+    // user can get user (him/herself)
 }
