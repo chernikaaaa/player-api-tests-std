7449bea49894bab0da74f485a8586722ed777562
veronika.cherkashchenko <veronika.cherkashchenko@mythical.games>
2025-10-23 12:02:21 +0200

    pass check

--- src/main/java/api/player/models/AllPlayersResponse.java
+++ src/main/java/api/player/models/AllPlayersResponse.java
@@ -2,6 +2,14 @@ package api.player.models;
 
 import java.util.List;
 
-public record AllPlayersResponse(List<Player> players) {
+public record AllPlayersResponse(List<AllPlayerResponseItem> players) {
+
+    public record AllPlayerResponseItem(Integer age,
+                                        String gender,
+                                        Integer id,
+                                        String role,
+                                        String screenName) {
+
+    }
 
 }
